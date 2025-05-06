d7601fb11d7de1e53a9b63837625693f44054269
SinaRastegari <sina.rastgar15@gmail.com>
2025-05-06 09:04:49 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* byte a;
+             byte b;
+             byte c;
+             Console.WriteLine("enter a");
+             a = byte.Parse(Console.ReadLine());
+             Console.WriteLine("enter b");
+             b = byte.Parse(Console.ReadLine());
+             Console.WriteLine("enter c");
+             c = byte.Parse(Console.ReadLine());
+             Console.Write(b * b - 4 * a * c);*/
+            /*  byte a;
+              byte b;
+              byte c;
+              Console.WriteLine("enter a");
+              a = byte.Parse(Console.ReadLine());
+              Console.WriteLine("enter b");
+              b = byte.Parse(Console.ReadLine());
+              Console.WriteLine("enter c");
+              c = byte.Parse(Console.ReadLine());
+              Console.Write((a + b + c) / 3); */
+            int a;
+            Console.Write("hoghogh ra vared kon");
+            a = int.Parse(Console.ReadLine());
+            Console.Write(a - (a * 0.03));
+
+
+
+
+        }
+
+        private static void write(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
